c3ab21969ba28d59ed01adf5670bef9ddbc52102
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2022-05-04 11:41:59 -0300

    adicionando diretorio oculto que estava faltando

--- camel-ws-to-event/src/main/java/com/redhat/ChatControllerRoute.java
+++ camel-ws-to-event/src/main/java/com/redhat/ChatControllerRoute.java
@@ -1,5 +1,7 @@
 package com.redhat;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class ChatControllerRoute extends RouteBuilder{
@@ -8,8 +10,14 @@ public class ChatControllerRoute extends RouteBuilder{
     public void configure() throws Exception {
         
         from("vertx-websocket://echo")
-            .log(">>> Message received from WebSocket Client : ${body}")
+            .log(">>> Message received from WebSocket Client : ${body} - ${headers}")
             .transform().simple("hello")
+            .process(new Processor() {
+                @Override
+                public void process(Exchange exchange) throws Exception {
+                    exchange.getIn();
+                }
+            })
         .to("vertx-websocket://echo");
 
     }
