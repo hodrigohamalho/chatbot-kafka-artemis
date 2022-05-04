5bff5aa56d4a83ec947b127a2f1c79bd7ecd6d45
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2022-05-04 16:07:51 -0300

    inicio da logica do wiretap pro broker

--- camel-ws-to-event/src/main/java/com/redhat/ChatControllerRoute.java
+++ camel-ws-to-event/src/main/java/com/redhat/ChatControllerRoute.java
@@ -15,12 +15,17 @@ public class ChatControllerRoute extends RouteBuilder{
         
         from("vertx-websocket://echo")
             .log(">>> Message received from WebSocket Client : ${body} - ${headers}")
+            .wireTap("direct:to-broker")
         .to("direct:send-to-kafka");
 
         from("direct:send-to-kafka").routeId("send-to-kafka")
             // .marshal().json()   // marshall message to send to kafka
             .setHeader("kafka.KEY", constant("Camel")) // Key of the message
         .to("kafka:"+KAFKA_TOPIC_RAW+"?brokers="+KAFKA_BOOTSTRAP_SERVERS);
+
+        from("direct:to-broker")
+            .log("sending message to message broker");
+            // .to("amqp:topic:chat-bot-raw");
 
         //Route that consumes message to kafka topic
         // from("kafka:"+KAFKA_TOPIC_RAW+"?brokers="+KAFKA_BOOTSTRAP_SERVERS+"&groupId=KAFKA_GROUP_ID")
