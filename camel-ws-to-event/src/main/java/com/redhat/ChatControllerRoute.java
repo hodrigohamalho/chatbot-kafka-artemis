102a657aa5237e2b701261e3d94fc2d4283c3304
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2022-05-04 16:03:42 -0300

    integracao com o kafka funcionando

--- camel-ws-to-event/src/main/java/com/redhat/ChatControllerRoute.java
+++ camel-ws-to-event/src/main/java/com/redhat/ChatControllerRoute.java
@@ -18,23 +18,15 @@ public class ChatControllerRoute extends RouteBuilder{
         .to("direct:send-to-kafka");
 
         from("direct:send-to-kafka").routeId("send-to-kafka")
-            .marshal().json()   // marshall message to send to kafka
+            // .marshal().json()   // marshall message to send to kafka
             .setHeader("kafka.KEY", constant("Camel")) // Key of the message
-            .to("kafka:"+ KAFKA_TOPIC_RAW + "?brokers=my-cluster-kafka-bootstrap:9092");
+        .to("kafka:"+KAFKA_TOPIC_RAW+"?brokers="+KAFKA_BOOTSTRAP_SERVERS);
 
-        // //Route that consumes message to kafka topic
-        // from("kafka:"+ KAFKA_TOPIC_RAW + "?brokers=" + KAFKA_BOOTSTRAP_SERVERS + "&groupId=" + KAFKA_GROUP_ID)
-        //     .routeId("kafkaConsumerRaw")
+        //Route that consumes message to kafka topic
+        // from("kafka:"+KAFKA_TOPIC_RAW+"?brokers="+KAFKA_BOOTSTRAP_SERVERS+"&groupId=KAFKA_GROUP_ID")
+        //     .routeId("kafka-consumer-raw")
         //     .unmarshal(new JacksonDataFormat(Message.class))
-        //     .log("Message received from Kafka Topic raw : ${body}")
-        //     .to("direct:insertProcessedTopic")
-        // ;
-
-        // //Route insert object on Processed Topic
-        // from("direct:insertProcessedTopic").routeId("insertProcessedTopic")        
-        // .to("kafka:"+ KAFKA_TOPIC_PROCESSED + "?brokers=" + KAFKA_BOOTSTRAP_SERVERS + "&groupId=" + KAFKA_GROUP_ID)
-        // .log("Message send from Kafka Topic processed : ${body}")
-        // ;
+        //     .log("Message received from Kafka Topic raw : ${body}");
 
     }
     
